536cc6fdf52d9f2e935e7853f77257cba7814c8c
timothymulatya <timothymulatya@gmail.com>
2025-11-09 20:49:16 +0300

    pushplant page initial state inclusion

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  // ---- STATE ----
+  const [plants, setPlants] = useState([]); // All plants from backend
+  const [searchTerm, setSearchTerm] = useState(""); // Text typed in the search box
+
+  
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={visiblePlants} />
     </main>
   );
 }
